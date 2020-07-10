47e1af0aa48d64ac85adda72690827c428f29012
Emma Potiron <emma.potiron@iseg.fr>
2020-07-10 12:44:32 +0200

    J10

---
+++ EX_02/afficher-produit.php
@@ -0,0 +1,18 @@
+<?php 
+   $pdo = new PDO("mysql:host=localhost;dbname=BaseTest01","user","1234"); 
+?> 
+
+<!DOCTYPE html>
+<html lang="fr">
+<body>
+    <ul>
+        <li>T-shirt coton de couleur rouge</li>
+        <li>T-shirt coton de couleur vert</li>
+        <li>ST-shirt coton de couleur argent</li>
+        <li>Short jean de couleur bleu</li>
+        <li>Short jean de couleur verte</li>
+        <li>Gilet de couleur noir</li>
+        <li>Veste de couleur argentée</li>
+    </ul>
+    </body>
+    </html>
